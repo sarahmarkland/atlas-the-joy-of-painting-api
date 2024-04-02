b6a3e2fda711dc9c5da1234af750ad9e4a41acb2
sarahmarkland <sarah.markland@icloud.com>
2024-04-02 18:58:59 -0500

    colors per episode, last line appended to top of csv bug

--- ETL/select_clean.py
+++ ETL/select_clean.py
@@ -5,27 +5,43 @@ import csv
 titles = []
 seasons = []
 episodes = []
+colors = []
 
 # open the Colors_Used.csv file
 with open('datasets/Colors_Used.csv', 'r', encoding='utf-8') as csvfile:
-    csvreader = csv.reader(csvfile)
+    csvreader = csv.DictReader(csvfile)
     # account for header row
-    headerRow = next(csvreader)
+    # headerRow = next(csvreader)
+    for row in csvreader:
+        title = row['painting_title']
+        season = row['season']
+        episode = row['episode']
+        color_list = row['colors']
 
     # strip titles and append to list
-    for row in csvreader:
-        title = row[3]
-        titles.append(title)
-        season = row[4]
-        seasons.append(season)
-        episode = row[5]
-        episodes.append(episode)
+    # for row in csvreader:
+    #     title = row[3]
+    #     titles.append(title)
+    #     season = row[4]
+    #     seasons.append(season)
+    #     episode = row[5]
+    #     episodes.append(episode)
 
-rows = zip(titles, seasons, episodes)
+
+        # convert string to list
+        color_list = color_list.strip("[]").replace("'", "").split(", ")
+
+        titles.append(title)
+        seasons.append(season)
+        episodes.append(episode)
+        colors.append(color_list)
+
+rows = zip(titles, seasons, episodes, colors)
+
 
 outputcsv = 'clean_data/squeaky.csv'
 with open(outputcsv, 'w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow([title, season, episode])
+    writer.writerow([title, season, episode, color_list])
     for row in rows:
         writer.writerow(row)
