485154d15cb72dd2c972ecf6c2410eb7ec9e808a
sarahmarkland <sarah.markland@icloud.com>
2024-04-02 18:48:20 -0500

    episode nums added

--- ETL/select_clean.py
+++ ETL/select_clean.py
@@ -4,6 +4,7 @@ import csv
 # store titles in a list
 titles = []
 seasons = []
+episodes = []
 
 # open the Colors_Used.csv file
 with open('datasets/Colors_Used.csv', 'r', encoding='utf-8') as csvfile:
@@ -17,12 +18,14 @@ with open('datasets/Colors_Used.csv', 'r', encoding='utf-8') as csvfile:
         titles.append(title)
         season = row[4]
         seasons.append(season)
+        episode = row[5]
+        episodes.append(episode)
 
-rows = zip(titles, seasons)
+rows = zip(titles, seasons, episodes)
 
 outputcsv = 'clean_data/squeaky.csv'
 with open(outputcsv, 'w', encoding='utf-8', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['title'])
-    for title in titles:
-        writer.writerow([title, season])
+    writer.writerow([title, season, episode])
+    for row in rows:
+        writer.writerow(row)
